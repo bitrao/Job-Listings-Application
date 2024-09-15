83ae29153522335b459fd8a242e673a632a7d1f6
tringuyenbao <96819287+tringuyenbao@users.noreply.github.com>
2024-09-15 21:28:28 +0700

    Add detail page component

--- routes.py
+++ routes.py
@@ -37,3 +37,22 @@ def category_page(category_url):
         )
     else:
         return "Category not found", 404
+
+
+@bp.route("/<category_url>/<int:entry_id>")
+def job_page(category_url, entry_id):
+    categories = load_categories()
+    category = next((cat for cat in categories if cat["url"] == category_url), None)
+    if category:
+        csv_file_path = os.path.join("data", category["file"])
+        try:
+            data = pd.read_csv(csv_file_path)
+            # Find the specific entry by its title
+            entry = data.loc[data["id"] == entry_id].to_dict("records")[0]
+        except (FileNotFoundError, IndexError):
+            entry = None  # Handle missing entry case
+        return render_template(
+            "job.html", categories=categories, category=category, entry=entry
+        )
+    else:
+        return "Category not found", 404
